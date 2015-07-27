68d3959f91cec7ba6ccf7071ee4dc2a6f60247b1
Alan Burke <alan@cycletraffic.com>
2015-07-27 22:16:38 +0100

    Add just js for colorbox and homepage map

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,19 @@
+$(document).ready(function(){
+    if (window.matchMedia) {
+      // Establishing media check
+        width700Check = window.matchMedia("(min-width: 700px)");
+        if (width700Check.matches){
+          $('a.gallery').colorbox({
+            rel:'gal',
+            scalePhotos: true,
+            maxWidth: '100%',
+            maxHeight: '100%'
+          });
+        }
+    }
 
+    // Stop Map scrolling automatically
+    $('.home-map').click(function () {
+      $('.home-map iframe').css("pointer-events", "auto");
+    });
+});
