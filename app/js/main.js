1cb86d84da7376018cb51d5e44752373d02c5314
Alan Burke <alan@cycletraffic.com>
2015-08-12 21:34:30 +0100

    Load lightbox at all screen widths - keep all in a single gallery

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,11 @@
 $(document).ready(function(){
-    if (window.matchMedia) {
-      // Establishing media check
-        width700Check = window.matchMedia("(min-width: 700px)");
-        if (width700Check.matches){
-          $('a.gallery').colorbox({
-            rel:'gal',
-            scalePhotos: true,
-            maxWidth: '100%',
-            maxHeight: '100%'
-          });
-          $('a.single').colorbox({
-            rel:'single',
-            scalePhotos: true,
-            maxWidth: '100%',
-            maxHeight: '100%'
-          });
-        }
-    }
+    // Load lightbox
+    $('a.gallery').colorbox({
+      rel:'gal',
+      scalePhotos: true,
+      maxWidth: '100%',
+      maxHeight: '100%'
+    });
 
     // Stop Map scrolling automatically
     $('.home-map').click(function () {
