41789f3b13db528f189d150d17d693dd53781fae
Alan Burke <alan@cycletraffic.com>
2015-09-03 12:11:11 +0100

    Download images for flooring and matresses

--- scripts/get-sources.sh
+++ scripts/get-sources.sh
@@ -10,6 +10,15 @@ wget https://dl.dropboxusercontent.com/u/$DROPBOX_UID/www.cfurniture.ie/sources/
 # Get Homepage image
 wget https://dl.dropboxusercontent.com/u/$DROPBOX_UID/www.cfurniture.ie/images/home.jpg -O app/media/images/categories/home.jpg
 
+# Get Flooring and Matress images
+wget https://dl.dropboxusercontent.com/u/$DROPBOX_UID/www.cfurniture.ie/images/flooring.jpg -O app/media/images/products/flooring.jpg
+wget https://dl.dropboxusercontent.com/u/$DROPBOX_UID/www.cfurniture.ie/images/flooring1.jpg -O app/media/images/products/flooring1.jpg
+wget https://dl.dropboxusercontent.com/u/$DROPBOX_UID/www.cfurniture.ie/images/flooring2.jpg -O app/media/images/products/flooring2.jpg
+wget https://dl.dropboxusercontent.com/u/$DROPBOX_UID/www.cfurniture.ie/images/matts.jpg -O app/media/images/products/matts.jpg
+wget https://dl.dropboxusercontent.com/u/$DROPBOX_UID/www.cfurniture.ie/images/matts1.jpg -O app/media/images/products/matts1.jpg
+wget https://dl.dropboxusercontent.com/u/$DROPBOX_UID/www.cfurniture.ie/images/matts2.jpg -O app/media/images/products/matts2.jpg
+
+
 in2csv $RAW/details.xls > $RAW/ranges-commas.csv
 # Replace pipes with br tags
 sed -ie 's/|/<br \/>/g' $RAW/ranges-commas.csv 
