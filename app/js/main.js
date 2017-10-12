a833a6b94c46c26e628be6491456819776367d1e
Alan Burke <alan@cycletraffic.com>
2017-10-12 18:56:39 +0100

    Use Main image to trigger Gallery - but be separate from it itself

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,11 @@ $(document).ready(function(){
       maxWidth: '100%',
       maxHeight: '100%'
     });
+    // Fire Lightbox gallery from Main image.
+    $('a.gallery-trigger').click(function(e){
+      e.preventDefault();
+      $('a.gallery').first().trigger('click');
+    });
 
     // Stop Map scrolling automatically
     $('.home-map').click(function () {
